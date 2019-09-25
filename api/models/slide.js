6db91331c549e217cd853e34a376a248390b000f
Jackson Kurtz <jkurtz678@gmail.com>
2019-09-24 21:42:14 -0700

    updated sample data to contain song names, authors, and urls

--- api/models/slide.js
+++ api/models/slide.js
@@ -1,7 +1,11 @@
 class Slide {
-    constructor(name, description) {
+    constructor(layout, name, description, song_url, song_name, song_author) {
+        this.layout = layout;
         this.name = name;
         this.description = description;
+        this.song_url = song_url;
+        this.song_name = song_name;
+        this.song_author = song_author;
     }
 }
 
--- data/PropulsionSlides.js
+++ data/PropulsionSlides.js
@@ -1,11 +1,51 @@
 const Slide = require('../api/models/slide.js');
 
-const title_slide = new Slide("Propulsion", "description for game...");
-const level_1 = new Slide("level 1", "battle theme");
-const level_2 = new Slide("level 2", "nebula");
-const level_3 = new Slide("level 3", "boss theme");
-const level_4 = new Slide("level 4", "epilogue");
+const Layouts = {
+    INTRO: 'intro',
+    CONTENT_1: 'content_1',
+    CONTENT_2: 'content_2',
+    CREDITS: 'credits'
+}
 
-const slides = [title_slide, level_1, level_2, level_3, level_4];
+const intro_slide = new Slide( Layouts.INTRO, "Intro" );
+
+const title_slide = new Slide(
+    Layouts.CONTENT_1,
+    "Propulsion", 
+    "description for game...",
+    "momentsgone.mp3", 
+    "moment's gone", 
+    "Harrison Kurtz"
+);
+
+const level_1 = new Slide(
+    Layouts.CONTENT_2,
+    "level 1", 
+    "battle theme", 
+    "sleep.mp3",
+    "sleep",
+    "Harrison Kurtz"
+);
+const level_2 = new Slide(
+    Layouts.CONTENT_1,
+    "level 2",
+    "nebula",
+    "offbalance.mp3",
+    "off balance", 
+    "Jackson Kurtz"
+);
+
+const level_3 = new Slide(
+    Layouts.CONTENT_2,
+    "level 3",
+    "boss theme",
+    "frominside.mp3",
+    "from inside",
+    "Harrison Kurtz"
+);
+
+const credits = new Slide(Layouts.CREDITS, "credits");
+
+const slides = [intro_slide, title_slide, level_1, level_2, level_3, credits];
 
 module.exports = slides;
